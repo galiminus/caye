0688a6fefc7597afda79f4827ecbee19ebfcf433
Victor Goya <goya.victor@gmail.com>
2014-09-06 17:38:32 +0800

    forget a few Nest calls

--- lib/extensions/store_messages.rb
+++ lib/extensions/store_messages.rb
@@ -2,7 +2,7 @@ class StoreMessages
   def incoming(message, callback)
     if (data = message["data"]) && data["to"] && data["from"]
       data["timestamp"] = Time.new.to_i
-      Nest.new("caye")[data["to"]][data["from"]].lpush data.to_json
+      Nest.new("caye", $redis)[data["to"]][data["from"]].lpush data.to_json
     end
     callback.call(message)
   end
--- lib/extensions/store_users.rb
+++ lib/extensions/store_users.rb
@@ -1,7 +1,7 @@
 class StoreUsers
   def incoming(message, callback)
     if message["data"] && message["data"]["from"]
-      Nest.new("caye")["users"].sadd message["data"]["from"]
+      Nest.new("caye", $redis)["users"].sadd message["data"]["from"]
     end
     callback.call(message)
   end
